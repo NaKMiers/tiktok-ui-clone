996e14de223d6fdbc1a7812ea8a59a1aa040040f
Pi Pi <diwas118151@gmail.com>
2022-09-21 17:24:20 +0700

    axios

--- src/components/Layouts/components/Search/index.js
+++ src/components/Layouts/components/Search/index.js
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from 'react'
+import aixos from 'axios'
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import HeadlessTippy from '@tippyjs/react/headless'
 import 'tippy.js/dist/tippy.css'
 
+import * as searchServices from '~/apiServices/searchServices'
 import AccountItem from '~/components/AccountItem'
 import { SearchIcon } from '~/components/Icons'
 import { PopperWrapper } from '~/components/Popper'
@@ -22,21 +24,14 @@ function Search() {
 
    useEffect(() => {
       if (debounced.trim()) {
-         setLoading(true)
+         const fetchApi = async () => {
+            setLoading(true)
 
-         fetch(
-            `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-               debounced
-            )}&type=less`
-         )
-            .then(res => res.json())
-            .then(res => {
-               setSearchResult(res.data)
-               setLoading(false)
-            })
-            .catch(() => {
-               setLoading(false)
-            })
+            const result = await searchServices.search(debounced)
+            setSearchResult(result)
+            setLoading(false)
+         }
+         fetchApi()
       } else {
          setSearchResult([])
          return
